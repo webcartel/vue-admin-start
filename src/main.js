6e051ff51fdce4b39d6281d569f6082cadd8b03a
webcartel <webcartel@yandex.ru>
2018-12-16 12:25:40 +0200

    install vuetify and material-design-icons-iconfont

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+Vue.use(Vuetify, {
+  iconfont: "mdidx"
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
